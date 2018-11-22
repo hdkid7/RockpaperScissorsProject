7124cc3448f1d6e5a6bf83e1a6a8691bcb9bd494
unknown <jujuk1d@protonmail.com>
2018-11-22 16:17:40 -0500

    Added rock paper scissors code. GUI will soon be released

---
+++ src/com/company/Main.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+// Random number generator
+        Random random = new Random();
+// Scanner
+        Scanner UserInput = new Scanner(System.in);
+
+        // 0 = Rock, 1 = paper, 2 = scissors
+        int UserChoice, ComputerChoice;
+
+        // Showing prompt
+        System.out.println("Welcome to the rock paper scissors game. Choose a value" +
+                " 0 = Rock, 1 = Paper, 2 = Scissors");
+        UserChoice = UserInput.nextInt();
+
+        if (UserChoice < 0 || UserChoice > 2) {
+            System.out.println("Invalid Option. Exiting program");
+            System.exit(0);
+        }
+        ComputerChoice = random.nextInt(3);
+
+        // Tie
+        if (UserChoice == ComputerChoice) {
+            if (UserChoice == 0){
+                System.out.println("Tie! Both players choose Rock");
+            } else if (UserChoice == 1) {
+                System.out.println("Tie! Both players choose Paper");
+            } else if (UserChoice == 2) {
+                System.out.println("Tie! Both players choose Scissors");
+            }
+        }
+
+        // If player chooses Rock
+
+        if (UserChoice == 0){
+            if (ComputerChoice == 1 ) {
+                System.out.println("You Lose! Computer choose paper. Paper beats rock!");
+            } else if (ComputerChoice == 2) {
+                System.out.println("You Win! Computer choose scissors. Rock beats scissors");
+            }
+        }
+        //If player chooses paper
+        if (UserChoice == 1) {
+            if (ComputerChoice == 0){
+                System.out.println("You win! Computer choose rock. Paper beats rock");
+            } else if (ComputerChoice == 2)
+                System.out.println("You lose. Computer choose scissors. Scissors beats paper!");
+        }
+        // If player chooses scissors
+        if (UserChoice == 2) {
+            if (ComputerChoice == 0) {
+                System.out.println("You lose! Computer choose rock. Rock beats scissors");
+            } else if (ComputerChoice == 1) {
+                System.out.println("You win! Computer choose paper. Scissors beats paper");
+            }
+
+        }
+
+    }
+
+
+}
